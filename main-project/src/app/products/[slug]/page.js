eb9116626e14294da22e6b0780648303c583d912
tanvirtbf <tanver018765@gmail.com>
2024-06-10 18:00:26 +0600

    project are dynamic now

--- main-project/src/app/products/[slug]/page.js
+++ main-project/src/app/products/[slug]/page.js
@@ -1,15 +1,19 @@
 import { CheckIcon } from "@heroicons/react/24/solid";
 import AddToCart from "../../../components/AddToCart";
 import ShareBtn from "../../../components/ShareBtn";
-const Product1 = () => {
+import { getProductbyId } from "../../../services/productService";
+import { formatAmount } from "../../../utils/stripe";
+const Product1 = async (props) => {
+  console.log(props.params.slug)
+  const product = await getProductbyId(props.params.slug)
   return (
     <div className="m-2 px-20">
       <div className="flex justify-around items-center flex-wrap">
         <div className="w-80 h-80">
-          <img src="/images/1.webp" className="w-full h-auto" />
+          <img src={product.images[0]} className="w-full h-auto" />
         </div>
         <div className="flex-1 max-w-md border rounded-md shadow-lg p-6 bg-white">
-          <h2 className="text-3xl font-semibold">Coding shirt</h2>
+          <h2 className="text-3xl font-semibold">{product.name}</h2>
           <div className="flex pt-2 gap-2">
             <CheckIcon className="text-lime-500 w-5 h-5" />
             <span className="font-semibold">In stock</span> |
@@ -17,12 +21,12 @@ const Product1 = () => {
           </div>
           <div className="mt-4 border-t pt-4">
             <p className="text-gray-500">Price:</p>
-            <p className="text-xl font-semibold">3000</p>
+            <p className="text-xl font-semibold">{formatAmount(product.default_price.unit_amount)}</p>
           </div>
           <AddToCart />
         </div>
       </div>
-      <p className="mt-8 text-2xl">this will contain product descriptionb</p>
+      <p className="mt-8 text-2xl">{product.description}b</p>
     </div>
   );
 };
--- main-project/src/services/productService.js
+++ main-project/src/services/productService.js
@@ -5,5 +5,15 @@ export const getProducts = async (limit) => {
     limit: 20,
     expand: ['data.default_price']
   })
+  console.log("------All Products----")
+  console.log(JSON.stringify(products,null,2))
   return products
+}
+export const getProductbyId = async (productId) => {
+  const product = await stripe.products.retrieve(productId,{
+    expand: ['default_price']
+  })
+  console.log("---------product-------------")
+  console.log(JSON.stringify(product,null,2))
+  return product
 }
