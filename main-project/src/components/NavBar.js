6e57c5335e607cadc5b80eb0a8a25454f35a0c95
tanvirtbf <tanver018765@gmail.com>
2024-06-10 18:11:32 +0600

    let's create another nav item

--- main-project/src/components/NavBar.js
+++ main-project/src/components/NavBar.js
@@ -1,18 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
-import {ShoppingCartIcon} from '@heroicons/react/24/outline'
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className='bg-white py-3 px-10 flex justify-between'>
-      <Link href={"/"} className='text-orange-500 font-bold text-3xl'>TanvirStore</Link>
+    <nav className="bg-white py-3 px-10 flex justify-between">
+      <Link href={"/"} className="text-orange-500 font-bold text-3xl">
+        TanvirStore
+      </Link>
       <Link href={"/cart"}>
-      <div className='text-orange-500 px-4 py-2 font-bold hover:text-red-600 cursor-pointer'>
-        <ShoppingCartIcon className='w-7 h-7 inline-block'/> Cart
-        <span>{0}</span>
-      </div></Link>
+        <div className="text-orange-500 px-4 py-2 font-bold hover:text-red-600 cursor-pointer">
+          <ShoppingCartIcon className="w-7 h-7 inline-block" /> Cart
+          <span>{0}</span>
+        </div>
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
